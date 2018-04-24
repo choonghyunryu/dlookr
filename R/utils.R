666d7e3d1341a39c5b0bbbc93db9f612e14f6e2c
choonghyunryu <choonghyun.ryu@gmail.com>
2018-04-24 22:35:27 +0900

    add get_os() function for OS discrimination

--- R/utils.R
+++ R/utils.R
@@ -284,3 +284,33 @@ find_skewness <- function(.data, index = TRUE, value = FALSE, thres = NULL) {
 }
 
 
+#' Finding Users Machine's OS
+#'
+#' @description
+#' Get the operating system that users machines.
+#'
+#' @return OS names. "windows" or "osx" or "linux"
+#'
+#' @examples
+#' get_os()
+#'
+#' @export
+get_os <- function() {
+  system_info <- Sys.info()
+
+  if (!is.null(system_info)) {
+    os <- system_info['sysname']
+
+    if (os == 'Darwin')
+      os <- "osx"
+  } else {
+    os <- .Platform$OS.type
+
+    if (grepl("^darwin", R.version$os))
+      os <- "osx"
+    if (grepl("linux-gnu", R.version$os))
+      os <- "linux"
+  }
+  tolower(os)
+}
+
